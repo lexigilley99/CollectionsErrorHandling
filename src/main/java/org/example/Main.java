e2ad0f404fb6677c37ec2b4db3736d73da2ee015
lexigilley99 <lexigilley99@gmail.com>
2024-07-31 09:57:58 -0400

    added list of numbers and arguements

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,45 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(5);
+        numbers.add(6);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Original list: " + numbers);
+
+        try {
+            // Removes the duplicates
+            Set<Integer> NumbersSet = new HashSet<>(numbers);
+            List<Integer> NumbersList = new ArrayList<>(NumbersSet);
+
+            // Average calculation
+            int sum = 0;
+            for (int num : NumbersList) {
+                sum += num;
+            }
+            double average = (double) sum / NumbersList.size();
+
+            System.out.println("Sum: " + sum);
+            System.out.println("Average: " + average);
+            System.out.println("List without duplicates: " + NumbersList);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Illegal argument: " + e.getMessage());
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("An unexpected error occurred: " + e.getMessage());
         }
     }
 }
